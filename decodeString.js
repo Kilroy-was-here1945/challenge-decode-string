adb55a1d9c03c707c21d82dd082edcea56138cce
Dallin Johnson <dallin.jofam@gmail.com>
2022-03-03 20:17:11 -0700

    do the thing

---
+++ decodeString.js
@@ -0,0 +1,20 @@
+// Write your code below this line
+// const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+// const index = 4;
+
+// console.log(`The character code ${sentence.charCodeAt(index)} is equal to ${sentence.charAt(index)}`);
+
+let string = '3hello'
+let num = []
+let preanswer = []
+for(let i = 0; i < string.length; i++){
+    if(string.charAt[i] === !isNaN){
+        num.push(string.charAt[i])
+    }
+preanswer.push(string.charCodeAt(i + num))
+}
+console.log(preanswer)
+for(let i = 1; i < preanswer.length; i++){
+    console.log(string.charAt(i + num))
+} 
